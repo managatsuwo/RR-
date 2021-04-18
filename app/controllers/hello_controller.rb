aa4d89ab22df0d8d4c0589b01b2466284fddbfad
“managatsuwo” <mystknr.macbook13@gmail.com>
2021-04-18 12:16:31 +0000

    125p コントローラーから

--- app/controllers/hello_controller.rb
+++ app/controllers/hello_controller.rb
@@ -1,21 +1,10 @@
 class HelloController < ApplicationController
-
+  layout 'hello'
   def index
-    if request.post? then
-      @title = 'Result'
-      if params['s1'] then
-        @msg = 'you selected: '
-        for val in params['s1']
-          @msg += val + ' '
-        end
-      else
-        @msg = 'not selected...'
-      end
-    else
-     @title = 'Index'
-     @msg = 'select radio button...'
-    end
-
+   @header = 'loyout sample'
+   @footer = 'copyright SYODA-Tuyano 2020.'
+   @title = 'New Layout'
+   @msg = 'this is sample page!'
   end
 
 end
